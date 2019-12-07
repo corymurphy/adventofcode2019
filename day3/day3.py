a59688f36782b9f8b2b3bd9f7fe730f527faff6b
Cory Murphy <cmurphy2266@gmail.com>
2019-12-07 17:52:04 -0500

    completed day 3 part 2

--- day3/day3.py
+++ day3/day3.py
@@ -48,16 +48,55 @@ def get_cross_sections(dict1, dict2):
         if (loc[0], loc[1]) in dict2:
             cross_sections.append((loc[0], loc[1]))
 
-    for loc in dict2.keys():
-        if (loc[0], loc[1]) in dict1:
-            if not (loc[0], loc[1]) in cross_sections:
-                cross_sections.append((loc[0], loc[1]))
+    # i dont think this is a case
+    #for loc in dict2.keys():
+    #    if (loc[0], loc[1]) in dict1:
+    #        if not (loc[0], loc[1]) in cross_sections:
+    #            cross_sections.append((loc[0], loc[1]))
     
     return cross_sections
+
+class CrossSection():
+    def __init__(self, location, wire1_steps, wire2_steps):
+        self.__location = location
+        self.__total_steps = wire1_steps + wire2_steps
+
+    @property
+    def total_steps(self):
+        return self.__total_steps
+
+    @property
+    def location(self):
+        return self.__location
+
+def get_cross_sections_with_steps(dict1, dict2):
+    cross_sections = []
+
+    for item in dict1.items():
+        key = item[0]
+        if key in dict2:
+            cross_section = CrossSection(key, dict1[key], dict2[key])
+            cross_sections.append(cross_section)
+
+    return cross_sections
+
+def get_least_steps_by_cross_sections(cross_sections):
+    shortest = 0
+    for i, cross_section in enumerate(cross_sections, start=0):
+
+        if i == 0:
+            shortest = cross_section.total_steps
+            continue
+
+        if cross_section.total_steps < shortest:
+            shortest = cross_section.total_steps
+
+    return shortest
 
 def travel(directions):
     visited = {}
     location = [0,0]
+    steps = 0
 
     for raw_path in directions:
 
@@ -68,26 +107,30 @@ def travel(directions):
         if heading == 'U':
             for i in range(distance):
                 y += 1
-                visited[x,y] = True
+                steps += 1
+                visited[x,y] = steps
             location[1] = y
             
         if heading == 'D':
             for i in range(distance):
                 y -= 1
-                visited[x,y] = True
+                steps += 1
+                visited[x,y] = steps
             location[1] = y
 
         # x axis left / right
         if heading == 'L':
             for i in range(distance):
                 x -= 1
-                visited[x,y] = True
+                steps += 1
+                visited[x,y] = steps
             location[0] = x
 
         if heading == 'R':
             for i in range(distance):
                 x += 1
-                visited[x,y] = True
+                steps += 1
+                visited[x,y] = steps
             location[0] = x
 
     return visited
@@ -103,8 +146,18 @@ def part_one(input_path='input'):
     
     return get_shortest_distance(cross_sections)
 
+def part_two(input_path='input'):
+    wire_directions = get_content(input_path)
+    wire1_directions = wire_directions[0].replace('\n','').split(',')
+    wire2_directions = wire_directions[1].replace('\n','').split(',')
+    wire1_visted = travel(wire1_directions)
+    wire2_visted = travel(wire2_directions)
+    cross_sections = get_cross_sections_with_steps(wire1_visted, wire2_visted)
+    return get_least_steps_by_cross_sections(cross_sections)
+
 def main(input_path='input'):
     print('part one: {}'.format(part_one(input_path)))
+    print('part two: {}'.format(part_two(input_path)))
 
 if __name__ == '__main__':
     main()
--- day3/test_day3.py
+++ day3/test_day3.py
@@ -40,6 +40,26 @@ class TestDay2(unittest.TestCase):
         actual = part_one('day3/input')
         self.assertEqual(expected, actual)
 
+    def test_shortest_steps0(self):
+        expected = 610
+        wire1_directions = 'R75,D30,R83,U83,L12,D49,R71,U7,L72'.split(',')
+        wire2_directions = 'U62,R66,U55,R34,D71,R55,D58,R83'.split(',')
+        wire1_visted = travel(wire1_directions)
+        wire2_visted = travel(wire2_directions)
+        cross_sections = get_cross_sections_with_steps(wire1_visted, wire2_visted)
+        actual = get_least_steps_by_cross_sections(cross_sections)
+        self.assertEqual(expected, actual)
+
+    def test_shortest_steps1(self):
+        expected = 410
+        wire1_directions = 'R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51'.split(',')
+        wire2_directions = 'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7'.split(',')
+        wire1_visted = travel(wire1_directions)
+        wire2_visted = travel(wire2_directions)
+        cross_sections = get_cross_sections_with_steps(wire1_visted, wire2_visted)
+        actual = get_least_steps_by_cross_sections(cross_sections)
+        self.assertEqual(expected, actual)
+
     def test_main(self):
         print("")
         print(" ------------------------- ")
@@ -61,4 +81,13 @@ wire2_directions = 'U62,R66,U55,R34,D71,R55,D58,R83'.split(',')
 wire1_visted = travel(wire1_directions)
 wire2_visted = travel(wire2_directions)
 cross_sections = get_cross_sections(wire1_visted, wire2_visted)
+cross_sections_steps = get_cross_sections_with_steps(wire1_visted, wire2_visted)
 actual = get_shortest_distance(cross_sections)
+
+
+a = cross_sections_steps[0]
+
+a = {}
+
+a[6,2] = 404
+a[14,5] = 404
